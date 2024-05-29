88a7ae41a2b4eb70cf7ca4a4cc54fba4bcb1083d
FelixCourrede <fcourrede@gmail.com>
2024-05-29 12:02:38 +0200

    gtex based on flaski; gtex imports working; needs UI test on aadatalake

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,11 @@
 # Copyright (c) Bioinformatics Core Facility of the Max Planck Institute for Biology of Ageing.
 # Distributed under the terms of the Modified BSD License.
-ARG MYAPP_IMAGE=mpgagebioinformatics/myapp:latest
+ARG MYAPP_IMAGE=mpgagebioinformatics/flaski:latest
 FROM $MYAPP_IMAGE
 
 LABEL maintainer "bioinformatics@age.mpg.de"
 
 COPY app.py /myapp/myapp/routes/index.py
 COPY _app.py /myapp/myapp/routes/_app.py
-
-RUN mkdir -p /flaski_private/aarnaseqlake/
-
-COPY requirements.txt /requirements.txt
-RUN pip3 install -r /requirements.txt
 
 USER myapp
--- app.py
+++ app.py
@@ -1,13 +1,36 @@
-from myapp import app, db, PAGE_PREFIX
-import dash
-from dash.dependencies import Input, Output, State
-from dash import dcc, html
-import dash_bootstrap_components as dbc
-from myapp.models import User
-from myapp.email import send_validate_email
-from datetime import datetime
-from ._utils import META_TAGS, check_email, password_check, navbar_A, protect_dashviews, make_navbar_logged
+from myapp import app, PAGE_PREFIX, PRIVATE_ROUTES
 from flask_login import current_user
+from flask_caching import Cache
+import plotly.graph_objects as go
+from flask import session
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output, State, MATCH, ALL
+from dash.exceptions import PreventUpdate
+from myapp.routes._utils import META_TAGS, navbar_A, protect_dashviews, make_navbar_logged
+import dash_bootstrap_components as dbc
+from myapp.routes.apps._utils import parse_import_json, parse_table, make_options, make_except_toast, ask_for_help, save_session, load_session, make_table, encode_session_app
+import os
+import uuid
+# import traceback
+import json
+import base64
+import pandas as pd
+# import time
+from werkzeug.utils import secure_filename
+import humanize
+from myapp.models import User
+import stat
+from datetime import datetime
+# import dash_table
+import shutil
+from time import sleep
+from myapp import db
+from myapp.models import UserLogging, PrivateRoutes
+from ._app import read_results_files, read_gene_expression, read_genes, read_significant_genes, \
+    filter_samples, filter_genes, filter_gene_expression, nFormat, read_dge,\
+        make_volcano_plot, make_ma_plot, make_pca_plot, make_annotated_col, make_bar_plot, plot_height
+        
 
 dashapp = dash.Dash("index",url_base_pathname=f'{PAGE_PREFIX}/', meta_tags=META_TAGS, server=app, external_stylesheets=[dbc.themes.BOOTSTRAP], title=app.config["APP_TITLE"], assets_folder=app.config["APP_ASSETS"])# , assets_folder="/flaski/flaski/static/dash/")
 
